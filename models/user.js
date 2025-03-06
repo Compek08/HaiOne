bb1963fbce63a5871394b48e6cc7bf5af1da4846
unknown <compek@dr.com>
2025-03-06 11:59:41 +0700

    Fix:Unique Username

--- models/user.js
+++ models/user.js
@@ -2,74 +2,75 @@
 const { Model } = require("sequelize");
 const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.hasOne(models.Profile);
-      this.hasMany(models.Transaction);
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.hasOne(models.Profile);
+            this.hasMany(models.Transaction);
+        }
     }
-  }
-  User.init(
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: { msg: "Username is required" },
-          notEmpty: { msg: "Username cannot be empty" },
+    User.init(
+        {
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    notNull: { msg: "Username is required" },
+                    notEmpty: { msg: "Username cannot be empty" },
+                },
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: { msg: "Must be a valid email" },
+                    notNull: { msg: "Email is required" },
+                    notEmpty: { msg: "Email cannot be empty" },
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "Password is required" },
+                    notEmpty: { msg: "Password cannot be empty" },
+                    len: {
+                        args: [6, 100],
+                        msg: "Password must be at least 6 characters",
+                    },
+                },
+            },
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: { msg: "Role is required" },
+                    notEmpty: { msg: "Role cannot be empty" },
+                    isIn: {
+                        args: [["user", "admin"]],
+                        msg: "Role must be either 'user' or 'admin'",
+                    },
+                },
+            },
         },
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: { msg: "Must be a valid email" },
-          notNull: { msg: "Email is required" },
-          notEmpty: { msg: "Email cannot be empty" },
-        },
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: { msg: "Password is required" },
-          notEmpty: { msg: "Password cannot be empty" },
-          len: {
-            args: [6, 100],
-            msg: "Password must be at least 6 characters",
-          },
-        },
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: { msg: "Role is required" },
-          notEmpty: { msg: "Role cannot be empty" },
-          isIn: {
-            args: [["user", "admin"]],
-            msg: "Role must be either 'user' or 'admin'",
-          },
-        },
-      },
-    },
-    {
-      sequelize,
-      modelName: "User",
-      hooks: {
-        beforeCreate: (user) => {
-          const salt = bcrypt.genSaltSync(10);
-          user.password = bcrypt.hashSync(user.password, salt);
-        },
-      },
-    }
-  );
+        {
+            sequelize,
+            modelName: "User",
+            hooks: {
+                beforeCreate: (user) => {
+                    const salt = bcrypt.genSaltSync(10);
+                    user.password = bcrypt.hashSync(user.password, salt);
+                },
+            },
+        }
+    );
 
-  return User;
+    return User;
 };
