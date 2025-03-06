59e28afb0f78e26d70da58a769e8cfc86ef7a91c
Wanda Budiono <wandaabudiono@gmail.com>
2025-03-06 09:59:30 +0700

    Compek'

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const { register, login, logout } = require("../controllers/authController");
 const { isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.use(isAdmin)
-router.get()
+router.use(isAdmin);
+// router.get()
 
 module.exports = router;
